1ff044534ba065624ea941281f45f82cd2cf0c2a
Carter Walsh <69424445+Carter-Walsh@users.noreply.github.com>
2021-04-21 20:39:07 -0500

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.unsplash.com",
     headers: {
-        Authorization: "Client-ID gt7muCnD917G-P09iIu0MAqguMN019rfBA8be1XkAfA"
+        Authorization: "enter-api-key-here"
     }
 });
 
